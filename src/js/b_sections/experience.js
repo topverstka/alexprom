e8e88c6ce615f41dc7056d07ad49e4412a090b9c
Boffeer <41575886+Boffeer@users.noreply.github.com>
2023-06-06 19:23:55 +0300

    Верстает статику блока с директором

---
+++ src/js/b_sections/experience.js
@@ -0,0 +1,29 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+
+	let experienceGallery = new Swiper(".experience-gallery", {
+    navigation: {
+      nextEl: ".experience-slides .swiper-button-next",
+      prevEl: ".experience-slides .swiper-button-prev",
+    },
+    grabCursor: true,
+    breakpoints: {
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      690: {
+        slidesPerView: 4,
+        spaceBetween: 24,
+      },
+      769: {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      },
+      1441: {
+        slidesPerView: 4,
+        spaceBetween: 31,
+      }
+    },
+  });
+
+});
--- src/js/index.js
+++ src/js/index.js
@@ -6,5 +6,7 @@ import "./libs/swiper-bundle.min.js";
 
 import "./scripts/scripts.js";
 
+import "./b_sections/experience.js";
+
 import "./b_components/groupers/bayan.js";
 
