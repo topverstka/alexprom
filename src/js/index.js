a803afa31f0a55564d9949d05413a7b0a2d40776
Boffeer <41575886+Boffeer@users.noreply.github.com>
2023-06-06 19:55:14 +0300

    Улучшает блок с директором и правит адаптивы

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,24 @@
 "use strict";
 
+/**
+ * Lazy Load
+ */
+import "./libs/lazyload.min.js";
+let lazyLoadInstance = new LazyLoad();
+
+/**
+ * Fancybox
+ */ 
+// import { Fancybox, Carousel } from "@fancyapps/ui";
+import { Fancybox, Carousel, Panzoom } from "@fancyapps/ui";
+Fancybox.bind('[data-fancybox]', {
+  Toolbar: {
+    display: [
+      "close",
+    ],
+  },
+});
+
 import "./b_components/header/burger.js";
 
 import "./libs/swiper-bundle.min.js";
